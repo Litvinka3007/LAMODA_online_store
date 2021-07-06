35c80101943051075990824ae839a57b63cb80a4
Angelina <angelina16161616@gmail.com>
2021-07-06 15:09:22 +0300

    Add functionality to the modal window

--- scripts/script.js
+++ scripts/script.js
@@ -10,4 +10,27 @@ headerCityButton.addEventListener('click', () => {
   const city = prompt('Укажите Ваш город');
   headerCityButton.textContent = city;
   localStorage.setItem('lamoda-location', city);
-})
+});
+
+// Модальное окно
+
+const subheaderCart = document.querySelector('.subheader__cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+
+const cartModalOpen = () => {
+  cartOverlay.classList.add('cart-overlay-open');
+};
+
+const cartModalClose = () => {
+  cartOverlay.classList.remove('cart-overlay-open');
+};
+
+subheaderCart.addEventListener('click', cartModalOpen);
+
+cartOverlay.addEventListener('click', event => {
+  const target = event.target;
+
+  if (target.matches('.cart__btn-close') || target.matches('.cart-overlay')) {
+    cartModalClose();
+  }
+});
